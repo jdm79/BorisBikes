3fb6ab3a5384fb0f0f20d020d8134afa0a77aa47
jdm79 <james.malvern@gmail.com>
2018-08-09 16:30:48 +0100

    added full? and empty? methods and added them to dock and release_bike methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,14 +8,22 @@ class DockingStation
   end
 
   def release_bike
-    raise "docking station is empty" unless @bikes.length > 0
-    @bikes  
+    @bikes unless empty?
   end
 
   def dock(bike)
-    raise "docking station is full" unless @bikes.length < 20
-    @bikes << bike
+    @bikes << bike unless full?
     @bikes[0]
+  end
+
+  private
+
+  def full?
+    raise "docking station is full" unless @bikes.length < 20
+  end
+
+  def empty?
+    raise "docking station is empty" unless @bikes.length > 0
   end
 
 end
