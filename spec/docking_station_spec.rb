06e33ad8a5342d172788c93a32b1ab779a8025b5
jdm79 <james.malvern@gmail.com>
2018-08-09 16:58:37 +0100

    refactored constant using name space operator

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -16,7 +16,7 @@ describe DockingStation do
   end
 
   it "checks if dock is full" do
-    20.times { subject.dock(Bike.new) }
+    DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new) }
     expect { subject.dock Bike.new }.to raise_error("docking station is full")
   end
 
